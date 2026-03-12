9d1bb65567aa144aa9352ed6dac313a15ddde829
Thushan Lokka <thushandrod24@gmail.com>
2026-03-12 08:00:58 +0530

    Update UserService.java

--- src/main/java/com/welfare/welfare_management/service/UserService.java
+++ src/main/java/com/welfare/welfare_management/service/UserService.java
@@ -347,6 +347,8 @@ public class UserService {
         otpExpiry.put(nic, System.currentTimeMillis() + (5 * 60 * 1000)); // 5 min expiry
         // Send OTP via email
         // emailService.sendOtpEmail(user.getEmail(), otp);
+        System.out.println("GN Request OTP "+ otp);
+        System
         // Fix #2: no trailing spaces in keys
         response.put("message", "OTP sent to your email");
         response.put("status", user.isActive());
